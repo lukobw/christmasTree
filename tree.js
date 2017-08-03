86256a933e52674d01756f421089e28a9427e6e7
Łukasz Obwarzanek <lobwarzanek@proaudio.local>
2017-08-03 14:11:47 +0200

    fix

--- tree.js
+++ tree.js
@@ -1,8 +1,8 @@
 function drawTree(n){
   for(i = 0; i < n; i++){
     var star = "";
-    for(j = 0; j < i; j++){
-      star = star += "*";
+    for(j = 0; j <= i; j++){
+      star += "*";
     }
     console.log(star);
   }
